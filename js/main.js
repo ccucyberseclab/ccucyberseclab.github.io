1e5d67709e80b4a15be6b25b21c91fb94b67d03c
Van-Linh Nguyen <tuonglinhhm@gmail.com>
2022-09-16 23:56:27 +0800

    Updated

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 /*
-* Template Name: BreezyCV - Resume / CV / vCard / Portfolio Template
-* Author: LMPixels
-* Author URL: http://themeforest.net/user/lmpixels
-* Version: 1.5.0
+* Author: John
 */
 
 (function($) {
